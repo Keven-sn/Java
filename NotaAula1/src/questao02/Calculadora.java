12939d06518e8110cd876fb53bbd798b441d39cb
Keven-sn <kevendouglas.sn@gmail.com>
2025-03-26 16:29:23 -0300

    Atividade

---
+++ NotaAula1/src/questao02/Calculadora.java
@@ -0,0 +1,26 @@
+package questao02;
+
+public class Calculadora {
+
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public double divisao(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            System.out.println("Erro: Divisão por zero!");
+            return Double.NaN;
+        }
+    }
+
+    public int multiplicacao(int a, int b) {
+        return a * b;
+    }
+}
